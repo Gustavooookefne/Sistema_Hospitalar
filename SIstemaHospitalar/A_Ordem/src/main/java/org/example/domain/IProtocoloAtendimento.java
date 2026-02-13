ebfc3d7c20c33f67bc2d1307f6c6f49c21f0bfd9
Igor Tayson Bresolin Savero <igor_savero@estudante.sesisenai.org.br>
2026-02-13 16:12:35 -0300

    feat: cria a entidade Paciente

--- SIstemaHospitalar/A_Ordem/src/main/java/org/example/domain/IProtocoloAtendimento.java
+++ SIstemaHospitalar/A_Ordem/src/main/java/org/example/domain/IProtocoloAtendimento.java
@@ -2,16 +2,7 @@ package org.example.domain;
 
 public interface IProtocoloAtendimento {
 
-    String priorizar (Paciente paciente);
+    String priorizar(Paciente paciente);
 
-    public class Paciente {
-
-        String nome;
-        public int frequenciaCardica;
-
-        public Paciente (String nome, int frequenciaCardica) {
-            this.nome = nome;
-            this.frequenciaCardica = frequenciaCardica;
-        }
-    }
 }
+
---
+++ SIstemaHospitalar/A_Ordem/src/main/java/org/example/domain/Paciente.java
@@ -0,0 +1,14 @@
+package org.example.domain;
+
+//Entidade
+public class Paciente {
+
+         String nome;
+        public int frequenciaCardica;
+
+        public Paciente (String nome, int frequenciaCardica) {
+            this.nome = nome;
+            this.frequenciaCardica = frequenciaCardica;
+
+        }
+}
