864c4fa9a1ce22147a1af9abb06dfddedd1c9769
Igor Tayson Bresolin Savero <igor_savero@estudante.sesisenai.org.br>
2026-02-13 16:28:44 -0300

    feat: inicia e termina a classe PainelNotification

---
+++ SIstemaHospitalar/A_Ordem/src/main/java/org/example/infra/PainelNotification.java
@@ -0,0 +1,11 @@
+package org.example.infra;
+
+public class PainelNotification {
+
+    public void chamaNoPainel (String mensagem){
+        System.out.println("------------------------------------");
+        System.out.println("NOTIFICAÇÂO: "+ mensagem);
+        System.out.println("------------------------------------");
+
+    }
+}
---
+++ SIstemaHospitalar/A_Ordem/src/main/java/org/example/service/TriagemService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+import org.example.domain.IProtocoloAtendimento;
+import org.example.domain.Paciente;
+import .
+public class TriagemService {
+}
