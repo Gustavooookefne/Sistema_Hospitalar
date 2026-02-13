1fbbc6f471ae2f532fcb5cdf076900dc29301af5
Igor Tayson Bresolin Savero <igor_savero@estudante.sesisenai.org.br>
2026-02-13 16:25:03 -0300

    feat: inicia e termina a classe ProtocoloUrgencia

---
+++ SIstemaHospitalar/A_Ordem/src/main/java/org/example/infra/ProtocoloUrgencia.java
@@ -0,0 +1,13 @@
+package org.example.infra;
+
+import org.example.domain.IProtocoloAtendimento;
+import org.example.domain.Paciente;
+
+public class ProtocoloUrgencia implements IProtocoloAtendimento {
+
+    @Override
+    public String priorizar (Paciente paciente) {
+
+        return "AMARELO! Atendimento em até 1 hora para: " + paciente.nome;
+    }
+}
