f034cf827e27302f8f8a2833f593e043454e7088
Gustavooookefne <gustavooookefne14@gmail.com>
2026-02-17 18:57:14 -0300

    feat: Finalizando trabalho

--- SIstemaHospitalar/O_Caos/src/main/java/org/example/br/com/sistema/infra/EmergenciaStrategy.java
+++ SIstemaHospitalar/O_Caos/src/main/java/org/example/br/com/sistema/infra/EmergenciaStrategy.java
@@ -1,5 +1,9 @@
 package org.example.br.com.sistema.infra;
 
-public class EmergenciaStrategy {
+import org.example.br.com.sistema.domain.IAtendimentoStrategy;
 
+public class EmergenciaStrategy implements IAtendimentoStrategy {
+    public void verificarUrgencia() { System.out.println("Verificando sinais vitais..."); }
+    public void solicitarExameLaboratorial() { /* Vazio - Violação de ISP */ }
+    public void executar() { System.out.println("Realizando atendimento de EMERGÊNCIA!"); }
 }
