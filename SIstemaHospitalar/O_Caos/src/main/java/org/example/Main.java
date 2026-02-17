9fc7e7d139faa56dab9b218fe11c79335d04cfac
Gustavooookefne <gustavooookefne14@gmail.com>
2026-02-17 19:01:57 -0300

    Finalizei o Service e o main

--- SIstemaHospitalar/O_Caos/src/main/java/org/example/Main.java
+++ SIstemaHospitalar/O_Caos/src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.br.com.sistema.domain.Paciente;
+import org.example.br.com.sistema.service.AtendimentoService;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner leitor = new Scanner(System.in);
+        Paciente p = new Paciente();
+        AtendimentoService service = new AtendimentoService();
+
+        System.out.println("--- SISTEMA HOSPITALAR (VERSÃO CAOS) ---");
+
+        System.out.print("Digite o nome completo do paciente: ");
+        p.setNomeCompleto(leitor.nextLine());
+
+        System.out.print("Digite o tipo de atendimento (EMERGENCIA ou EXAME): ");
+        String tipo = leitor.nextLine().toUpperCase();
+
+        System.out.println("\nProcessando...");
+
+        service.iniciar(tipo, p);
+
+        leitor.close();
     }
 }
--- SIstemaHospitalar/O_Caos/src/main/java/org/example/br/com/sistema/infra/MySQLRepository.java
+++ SIstemaHospitalar/O_Caos/src/main/java/org/example/br/com/sistema/infra/MySQLRepository.java
@@ -1,5 +1,9 @@
 package org.example.br.com.sistema.infra;
 
-public class MySQLRepository {
+import org.example.br.com.sistema.domain.IRepository;
 
+public class MySQLRepository implements IRepository {
+    public void salvar(Object obj) {
+        System.out.println("Salvando dados no MySQL: " + obj.toString());
+    }
 }
