85f656c2f2cc7760fb38218de7be6138a13c48eb
Gustavooookefne <gustavooookefne14@gmail.com>
2026-03-05 13:00:31 -0300

    feat: Adicionando comentarios no modelo caos

--- SIstemaHospitalar/O_Caos/src/main/java/org/example/Main.java
+++ SIstemaHospitalar/O_Caos/src/main/java/org/example/Main.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 
 class Paciente {
     public String nomeCompleto;
-
 }
-
 
 // Note que as classes serão forçadas a implementar coisas que não precisam
 interface IAtendimentoStrategy {
@@ -23,11 +21,14 @@ class MySQLRepository {
     }
 }
 
-// --- SERVIÇO COM O "CAOS" (Viola DIP, OCP e LSP) ---
+// --- SERVIÇO COM O "CAOS" (Viola DIP , OCP e SRP) ---
+// a class AtendimentoService esta sendo sobrecaregada sendo utilizada para decidir o fluxo(triagem), executa a logica, e salva no banco
 class AtendimentoService {
 
+    // Por conta que o DIP ele esta "preso" ao MYSQL, caso eu queira trocar de banco de dados, eu vou precisar trocar o codigo todo
     private MySQLRepository repository = new MySQLRepository();
 
+    // estou utilizando o if/else para tipoAtendimento, caso eu crie um outro tipo de atindimento eu vou ter que adicionar mais um else if
     public void iniciar(String tipoAtendimento, Paciente paciente) {
 
         if (tipoAtendimento.equals("EMERGENCIA")) {
@@ -40,17 +41,20 @@ class AtendimentoService {
             System.out.println("Coletando sangue de: " + paciente.nomeCompleto);
         }
 
-
         repository.salvarNoBanco(paciente);
         System.out.println("Atendimento finalizado.");
     }
 }
 
 // --- IMPLEMENTAÇÕES "CAPENGAS" (Violação de ISP e LSP) ---
+// por conta que eu estou forçando a class emergenciaStrategy a herdar o metodo solicitarExameLaboratorio, a emergencia
+// não faz esse tipo d e solicitação, a class fica poluida
 class EmergenciaStrategy implements IAtendimentoStrategy {
     public void verificarUrgencia() { System.out.println("Sinais OK."); }
     public void solicitarExameLaboratorial() {
 
+        //O LSP é quebrado por conta da class filha (emergencia) q vai lançar uma execao em um meto que a interface obrigou a ter
+        // o q vai crachar nosso sistema ao tentar usar uma class generica
         throw new UnsupportedOperationException("Emergência não pede exame laboratorial aqui.");
     }
     public void executar() { System.out.println("Atendendo!"); }
